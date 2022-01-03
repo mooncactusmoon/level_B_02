082e9d1db4c29c9b55459d24f924d6f765c3cc0c
MoonLin <mooncactusmoon@gmail.com>
2022-01-03 09:28:55 +0800

    marquee

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 <title>健康促進網</title>
-<link href="./home_files/css.css" rel="stylesheet" type="text/css">
+<link href="./css/css.css" rel="stylesheet" type="text/css">
 <script src="./js/jquery-1.9.1.min.js"></script>
 <script src="./js/js.js"></script>
 </head>
@@ -12,12 +12,14 @@
 <div id="alerr" style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
 	<pre id="ssaa"></pre>
 </div>
-<iframe name="back" style="display:none;"></iframe>
+
 	<div id="all">
     	<div id="title">
-        00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36        </div>
+        00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36        
+		<a href="index.php" style="float:right">回首頁</a>
+		</div>
         <div id="title2">
-        	
+        	<a href="index.php" title="健康促進網-回首頁"><img src="./icon/02B01.jpg" alt="健康促進網-回首頁" ></a>
         </div>
         <div id="mm">
         	<div class="hal" id="lef">
@@ -29,7 +31,8 @@
                	                 </div>
             <div class="hal" id="main">
             	<div>
-            		
+					<!-- 有預設的padding margin等等 所以marquee的width要<82% -->
+            		<marquee style="width:80%; display:inline-block;">請民眾踴躍投稿電子報，讓電子報成為大家互相交流、分享的園地!詳見最新文章</marquee>
                 	<span style="width:18%; display:inline-block;">
                     	                    	<a href="?do=login">會員登入</a>
                     	                    </span>
