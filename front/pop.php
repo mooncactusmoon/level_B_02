d7854368f2ae51b1ea1b4738f26ab846af114358
MoonLin <mooncactusmoon@gmail.com>
2022-01-07 13:27:59 +0800

    完成人氣文章

--- front/pop.php
+++ front/pop.php
@@ -22,41 +22,70 @@
                 </td>
                 <td class="switch">
                     <div class="short"><?= mb_substr($row['text'], 0, 20); ?>...</div>
-                    <div class="full" style="display:none;"><?= nl2br($row['text']); ?></div>
+                    <div class="full pop" style="display:none;">
+                        <h2 style="color:skyblue;">
+                            <?php
+                                $tarray=[
+                                    "1"=>"健康新知",
+                                    "2"=>"菸害防治",
+                                    "3"=>"癌症防治",
+                                    "4"=>"慢性病防治",
+                                ];
+                                echo $tarray[$row['type']]; //陣列寫法
+
+                            // //    switch寫法
+                            // switch ($row['type']) {
+                            //     case 1:
+                            //         echo "健康新知";
+                            //     break;
+                            //     case 2:
+                            //         echo "菸害防治";
+                            //     break;
+                            //     case 3:
+                            //         echo "癌症防治";
+                            //     break;
+                            //     case 4:
+                            //         echo "慢性病防治";
+                            //     break;
+                            // }
+                            ?>
+                        </h2>
+                        <?= nl2br($row['text']); ?>
+                    </div>
                 </td>
-                <td><?=$row['good'];?>個人說<img src="icon/02B03.jpg" width="25px" height="25px"></td>
+                <td><?= $row['good']; ?>個人說<img src="icon/02B03.jpg" width="25px" height="25px"></td>
             </tr>
         <?php
         }
         ?>
     </table>
     <div>
-    <?php
-    if(($now-1)>0){
-        $prev=$now-1;
-        echo "<a href='index.php?do=news&p=$prev'>";
-        echo "<";
-        echo "</a>";
-    }
-    for($i=1;$i<=$pages;$i++){
-        $font=($now==$i)?'24px':'16px';
-        echo "<a href='index.php?do=news&p=$i' style='font-size:$font'>";
-        echo $i;
-        echo "</a>";
-    }
-    if(($now-1)<=$pages){
-        $next=$now+1;
-        echo "<a href='index.php?do=news&p=$next'>";
-        echo ">";
-        echo "</a>";
-    }
-    ?>
+        <?php
+        if (($now - 1) > 0) {
+            $prev = $now - 1;
+            echo "<a href='index.php?do=pop&p=$prev'>";
+            echo "<";
+            echo "</a>";
+        }
+        for ($i = 1; $i <= $pages; $i++) {
+            $font = ($now == $i) ? '24px' : '16px';
+            echo "<a href='index.php?do=pop&p=$i' style='font-size:$font'>";
+            echo $i;
+            echo "</a>";
+        }
+        if (($now - 1) <= $pages) {
+            $next = $now + 1;
+            echo "<a href='index.php?do=pop&p=$next'>";
+            echo ">";
+            echo "</a>";
+        }
+        ?>
     </div>
 </fieldset>
 <script>
-$(".switch").hover(
-    function(){
-    $(this).parent().find(".short,.full").toggle()
-})
-
+    $(".switch").hover(
+        function() {
+            // $(this).parent().find(".full").addClass('pop').toggle();
+            $(this).parent().find(".pop").toggle();
+        })
 </script>
