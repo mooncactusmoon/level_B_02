6976f5a761b887531b8bda590a0ee14caa9dd528
MoonLin <mooncactusmoon@gmail.com>
2022-01-10 10:31:39 +0800

    完成後台新增問卷功能

---
+++ api/que.php
@@ -0,0 +1,22 @@
+<?php
+include_once "../back.php";
+
+
+    $subject=$_POST['subject'];
+    $Que->save(['text'=>$subject,'parent'=>0,'count'=>0]);
+    
+    // $parent_id=$Que->math("max","id");
+    $parent_id=$Que->find(['text'=>$subject])['id'];
+    echo $parent_id;
+
+
+    foreach($_POST['options'] as $opt){
+
+        $Que->save(['text'=>$opt,'parent'=>$parent_id,'count'=>0]);
+
+    }
+
+    to("../back.php?do=que");
+
+
+?>
--- back/que.php
+++ back/que.php
@@ -1,1 +1,28 @@
-que
+<fieldset>
+    <legend>新增問卷</legend>
+    <form action="api/que.php" method="post">
+        <div style="display:flex;">
+            <div class="clo">問卷名稱</div>
+            <div><input type="text" name="subject"></div>
+        </div>
+        <div class="clo" id="opt">
+            <div>
+
+                <span>選項</span>
+                <input type="text" name="options[]">
+                <input type="button" value="更多" onclick="more()">
+            </div>
+        </div>
+        <div>
+            <input type="submit" value="新增">|
+            <input type="reset" value="清空">
+        </div>
+    </form>
+</fieldset>
+<script>
+    function more(){
+       let opt=`<div><span>選項</span><input type="text" name="options[]"></div>`
+       $("#opt").prepend(opt);
+    }
+
+</script>
